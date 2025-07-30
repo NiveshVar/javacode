d31f2e2df19b1b04cf54708bbded76c4899a091d
Nivesh Varun <163310775+NiveshVar@users.noreply.github.com>
2025-07-30 23:10:38 +0530

    Create java1.java

---
+++ java1.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class LearnStatement {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        String subject = scanner.nextLine();
+
+        System.out.println("May I know how to learn " + subject + "!!!...");
+    }
+}
